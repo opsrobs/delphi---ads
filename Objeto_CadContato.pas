983c65264e44663bd989f5d8cc3adeb8fce0a1ea
robson.flavio20@outlook.com <opsrobs>
2022-05-09 16:53:32 -0300

    Objeto_CadContato.pas foi criado

---
+++ Objeto_CadContato.pas
@@ -0,0 +1,69 @@
+unit Objeto_CadContato;
+
+interface
+  uses Objeto_CadPessoa;
+
+  type CadContato = class (CadPessoa)
+
+  private
+    idStatus_telefone:integer;
+    status_contato:Boolean;
+    contato:string[45];
+    Pessoa_idPessoa:integer;
+  public
+    procedure setIdStatus_Telefone(idStatus_Telefone:integer);
+    function getIdStatus_Telefone:Integer;
+    procedure setStatus_contato(status_contato:Boolean);
+    function getSattus_contato:Boolean;
+    procedure setContato(contato:string);
+    function getContato:string;
+    procedure setPessoa_idPessoa(Pessoa_idPessoa:integer);
+    function getPessoa_idPessoa:integer;
+  end;
+
+
+implementation
+
+{ CadContato }
+
+function CadContato.getContato: string;
+begin
+
+end;
+
+function CadContato.getIdStatus_Telefone: Integer;
+begin
+
+end;
+
+function CadContato.getPessoa_idPessoa: integer;
+begin
+
+end;
+
+function CadContato.getSattus_contato: Boolean;
+begin
+
+end;
+
+procedure CadContato.setContato(contato: string);
+begin
+
+end;
+
+procedure CadContato.setIdStatus_Telefone(idStatus_Telefone: integer);
+begin
+
+end;
+
+procedure CadContato.setPessoa_idPessoa(Pessoa_idPessoa: integer);
+begin
+
+end;
+
+procedure CadContato.setStatus_contato(status_contato: Boolean);
+begin
+
+end;
+
+end.
